d6913f939ea9a0ae056befd06d6533166841d737
Nazar Kovalenko <xeilen1112@gmail.com>
2022-05-21 14:04:09 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Enter file name: ', (fileName) => {
+  rl.question('Enter file content: ', (content) => {
+    fs.appendFile('./src/' + fileName + '.txt', content, (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log('Error');
+
+        return rl.close();
+      }
+
+      // eslint-disable-next-line no-console
+      console.log('Success');
+
+      return rl.close();
+    });
+  });
+});
